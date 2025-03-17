10970eede48bfacd650b90098a53b14c6de3d801
shubh-codes7 <shubhamcodes7@gmail.com>
2025-03-17 09:00:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let elm = document.querySelector("#level");
+
+function count(x){
+    if(x.parentElement === null) return 1;
+    return 1 + count(x.parentElement);
+}
+
+let n = count(elm);
+alert(`The level of the element is: ${n}`)
